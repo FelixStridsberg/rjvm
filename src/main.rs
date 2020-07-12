36818cf5e18da82b5eef01e0766f27fe1f0e780a
Felix Stridsberg <felixstridsberg@gmail.com>
2020-07-12 16:49:04 +0200

    Simple main class for easy testing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use rjvm::vm::VirtualMachine;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let class_name = &args[1];
+    let method_name = &args[2];
+
+    let mut file = class_name.clone();
+    file.push_str(".class");
+
+    let mut vm = VirtualMachine::default();
+    vm.register_class(&file).unwrap();
+
+    let return_value = vm.run(class_name, method_name, vec![]);
+    println!("Returned {:?}", return_value);
 }
